68b92a526776355aafcbb9ecff0186795753499e
David Diienno <daveedno@gmail.com>
2017-01-25 17:42:36 -0500

    Implemented Inventory GUI
    created a visually ugly inventory GUI (we can make it look better later)
    
    * Finds all inventory slots and changes their text to represent no
    items on creation
    
    * Bring up the Inventory by pressing the “i” key
    
    *TODO- Still have to implement actually adding and removing items, but
    the methods are there
    
    scripts are in scripts folder

---
+++ HFSurvivalHorrorRPG/Assets/Scripts/Inventory.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//TODO Serialize this to be preserved through saves
+//TODO debug ArrayList not accepting generic types
+using UnityEngine.UI;
+
+
+public class Inventory : MonoBehaviour {
+	public ArrayList playerInventory;
+	private const int MAX_INV_SIZE = 8;
+	private int currentItems = 0;
+	public Canvas inventoryUI;
+
+	// Use this for initialization
+	void Start () {
+		inventoryUI = GameObject.FindGameObjectWithTag("Canvas").GetComponent<Canvas>();
+		playerInventory = new ArrayList();
+		InitializeGUI (inventoryUI);
+	}
+
+	void Update(){
+		if(Input.GetKeyDown(KeyCode.I)){
+			inventoryUI.enabled = !inventoryUI.enabled;
+		}
+	}
+
+	/**
+	 * Method to add an item found in game to the inventory 
+	 * Inventory must have atleast one free space
+	 * @param Item - found item to be added
+	 * @returns true if item was added to inventory
+	 */
+	private bool addItem(Item foundItem){
+		if (currentItems < MAX_INV_SIZE) {
+			playerInventory.Add (foundItem);
+			currentItems++;
+			return true;
+		} else {
+			Debug.Log ("Inventory is full");
+		}
+		return false;
+
+	}
+
+	/**
+	 * Remove method mainly used when disposing of an item thats is not usefull to the player
+	 * @params index - the index of the inventory where the specific item is located
+	 */ 
+	private void removeItem(int index){
+		if (currentItems > 0) {
+			playerInventory.RemoveAt(index);
+			currentItems--;
+		} else {
+			Debug.Log ("There is nothing in your inventory to remove");
+		}
+	}
+
+	/**
+	 * Remove method used for crafting and removing duplicates of an item.
+	 */
+	private Item removeDuplicates(int index){
+		Item removingItem = (Item) playerInventory [index];
+		if (removingItem.numOfItem > 1) { // remove duplicates
+			removingItem.setNumOf (removingItem.numOfItem - 1);
+			return removingItem;
+		} else { // no more duplicates, remove from inventory
+			removeItem (index);
+			return removingItem;
+		}
+	}
+
+	/**
+	 * Method to set the initial Inventory GUI, sets all buttons to display "No Item"
+	 * 
+	 * Finds all children of the canvas by tag"InvSlot" and goes into their children and
+	 * looks for a text component to manipulate
+	 * TODO - implement adding and removing items
+	 * @params GUI canvas housing buttons
+	 */ 
+	private void InitializeGUI(Canvas canvas){
+		for (int i = 0; i < canvas.transform.childCount; i++) {
+			Transform child = canvas.transform.GetChild (i);
+			if (child.tag == "InvSlot") {
+				Transform childsChild = child.transform.GetChild (0);
+				Text bText = childsChild.GetComponent<Text> ();
+				bText.text = "No Item";
+
+			}
+		}
+		//disable GUI visual
+		inventoryUI.enabled = false;
+	}
+}
---
+++ HFSurvivalHorrorRPG/Assets/Scripts/Item.cs
@@ -0,0 +1,42 @@
+﻿/**
+ * @Author David Diienno
+ * 1/23/17
+ * Item class to be used in unison with the players inventory
+*/
+
+public class Item{
+	public string itemName;
+	private string itemID;
+	public int numOfItem = 0;
+	/**
+	 *@params name - String to determine the items title
+	 *@params id - Unique ID to the item, used for equals, and crafting later.
+	 */
+	public Item(string name, string id){
+		itemName = name;
+		itemID = id;	
+	}
+	// TODO - implement constructor to create an item based off the item ID (Data class using hashmap(itemName, ID))
+
+
+	//Accessor & Mutator Methods
+	public string getName(){
+		return itemName;
+	}
+	public string getID(){
+		return itemID;
+	}
+	public int getNumOf(){
+		return numOfItem;
+	}
+	public void setName(string name){
+		itemName = name;
+	}
+	public void setID(string id){
+		itemID = id;
+	}
+	public void setNumOf(int num){
+		numOfItem = num;
+	}
+
+}
