1356a15edeb5d9c1f00d69a1be9132b14ebcbfd2
David Diienno <daveedno@gmail.com>
2017-02-02 14:26:45 -0500

    PlayerChan fix
    Player chan now walks upon wasd and runs when left shit is pressed

--- HFSurvivalHorrorRPG/Assets/Standard Assets/Characters/ThirdPersonCharacter/Scripts/ThirdPersonUserControl.cs
+++ HFSurvivalHorrorRPG/Assets/Standard Assets/Characters/ThirdPersonCharacter/Scripts/ThirdPersonUserControl.cs
@@ -114,12 +114,13 @@ namespace UnityStandardAssets.Characters.ThirdPerson
             float v = CrossPlatformInputManager.GetAxis("Vertical");
             bool crouch = Input.GetKey(KeyCode.C);
 
+
             // calculate move direction to pass to character
             if (m_Cam != null)
             {
                 // calculate camera relative direction to move:
                 m_CamForward = Vector3.Scale(m_Cam.forward, new Vector3(1, 0, 1)).normalized;
-                m_Move = v*m_CamForward + h*m_Cam.right;
+				m_Move = (v*m_CamForward + h*m_Cam.right) * 0.5f;
             }
             else
             {
@@ -128,7 +129,7 @@ namespace UnityStandardAssets.Characters.ThirdPerson
             }
 #if !MOBILE_INPUT
 			// walk speed multiplier
-	        if (Input.GetKey(KeyCode.LeftShift)) m_Move *= 0.5f;
+	        if (Input.GetKey(KeyCode.LeftShift)) m_Move /= 0.5f;
 #endif
 
             // pass all parameters to the character control script
